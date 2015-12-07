39d820a000bf10959dc4562445018c412d02c6ff
dalmo@osx <kristian@doingit.no>
2015-12-08 00:48:48 +0100

    Better tests

--- tests/testthat/test-attributes.R
+++ tests/testthat/test-attributes.R
@@ -6,9 +6,17 @@ y <- list("config" = setNames(c("no", NA, "new"), c("foo", "bar", "zoo")))
 test_that("Updating attributes works", {
   z <- update_named_attr(x$config, y$config)
   expect_identical(z, c(x$config[1:2], y$config[3]))
+
+  z <- update_named_attr(y$config, x$config)
+  expect_identical(z, c(y$config[1], x$config[2], y$config[3]))
+
 })
 
 test_that("Merging attributes works", {
   z <- merge_attributes(list(x, y))
   expect_identical(z$config, c(x$config[1:2], y$config[3]))
+
+  z <- merge_attributes(list(y, x))
+  expect_identical(z$config, c(y$config[1], x$config[2], y$config[3]))
+
 })
