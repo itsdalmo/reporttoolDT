15d27daf3559f65e6ebd27a5d916efa0a357cc85
dalmo@surface <kristian@doingit.no>
2016-02-16 16:32:43 +0100

    Fix

--- test.R
+++ test.R
@@ -8,57 +8,54 @@ x <- reporttool::read_data("./test.sav")
 # R6 -------------
 
 Survey <- R6::R6Class("Survey",
-                      private = list(
-                        .associations = NULL,
-                        .labels = NULL,
-                        .config = NULL,
-                        .dictionary = NULL
-                      ),
-                      public = list(
-                        data = NULL,
+  private = list(
+    .associations = NULL,
+    .labels = NULL,
+    .config = NULL,
+    .dictionary = NULL
+  ),
+  public = list(
+    .data = NULL,
 
-                        initialize = function(df) {
-                          if (missing(df) || !is.data.frame(df))
-                            stop("Expecting a data.frame or data.table.", call = FALSE)
-                          # if (contains_labelled(df))
-                            # df <- convert_labelled(df)
-                          self$data <- df
-                          private$.labels <- attr(df, "labels")
-                        },
+    initialize = function(df) {
+      if (missing(df) || !is.data.frame(df))
+        stop("Expecting a data.frame or data.table.", call = FALSE)
+      self$.data <- df
+    },
 
-                        bracket = function(...) {
-                          `[`(self$data, ...)
-                        },
+    bracket = function(...) {
+      `[`(self$.data, ...)
+    },
 
-                        dbracket = function(...) {
-                          `[[`(self$data, ...)
-                        },
+    dbracket = function(...) {
+      `[[`(self$.data, ...)
+    },
 
-#                         dollar = function(name) {
-#                           `$`(self$data, name)
-#                         },
-#
-                        bracket_repl = function(i, j, value) {
-                          `[<-`(self$data, i, j, value)
-                        },
+    bracket_repl = function(i, j, value) {
+      `[<-`(self$.data, i, j, value)
+    },
 
-                        dbracket_repl = function(i, j, value) {
-                          `[[<-`(self$data, i, j, value)
-                        },
-#
-#                         dollar_repl = function(name, value) {
-#                           `$<-`(self$data, name, value)
-#                         },
-#
-                        print = function(...) {
-                          print(self$data)
-                        }
-                      )
+    dbracket_repl = function(i, j, value) {
+      `[[<-`(self$.data, i, j, value)
+    },
+
+    print = function(...) {
+      print(self$.data)
+    }
+  )
 )
-
 
 survey <- function(df) Survey$new(df)
 
+Survey_dt <- R6::R6Class("Survey_dt",
+                         inherit = Survey,
+                         public = list(
+                           initialize = function(df) {
+                             self$.data <- data.table::as.data.table(df)
+                           }
+                         ))
+
+Survey_df <- R6::R6Class("Survey_df", inherit = Survey)
 
 `[.Survey` <- function(x, ...) {
   x$bracket(...)
@@ -68,11 +65,12 @@ survey <- function(df) Survey$new(df)
   x$dbracket(...)
 }
 
-# `$.Survey` <- function(x, name) {
-#   if (name != "data") {
-#     x$data[[name]]
+# `$.Survey_dt` <- function(x, name) {
+#   if (is.character(name) && substr(name, 0, 1) == ".") {
+#     # get(name, envir = x)
+#     NextMethod()
 #   } else {
-#     x$dollar(name)
+#     `[[`(x$.data, name)
 #   }
 # }
 
@@ -84,9 +82,9 @@ survey <- function(df) Survey$new(df)
   x$dbracket_repl(i, j, value)
 }
 
-# `$<-.Survey` <- function(x, name, value) {
-#   x$dollar_repl(name, value)
-# }
+# TEST
 
 y <- survey(x)
 y <- survey(data.table::as.data.table(x))
+
+y <- Survey_dt$new(df)
