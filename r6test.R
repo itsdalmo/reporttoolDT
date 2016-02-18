1b026f03e2bf85dcb7a506dbffd14163824ce262
dalmo@surface <kristian@doingit.no>
2016-02-18 13:22:33 +0100

    Moving forward

--- r6test.R
+++ r6test.R
@@ -1,75 +1,106 @@
 rm(list = ls(all = TRUE))
 x <- reporttool::read_data("./test.sav")
+# x <- reporttool::from_labelled(x)$df
 
 # R6 -------------
 #' @importFrom R6 R6Class
 Survey <- R6::R6Class("Survey",
   private = list(
+    .data = NULL,
     .associations = NULL,
     .labels = NULL,
     .config = NULL,
-    .dictionary = NULL
+    .dictionary = NULL,
+    .marketshares = NULL
   ),
   public = list(
-    .data = NULL,
 
     initialize = function(df) {
       if (missing(df) || !is.data.frame(df))
         stop("Expecting a data.frame or data.table.", call = FALSE)
-      self$.data <- df
+      private$.data <- df
+    },
+
+    update = function() {
+
+      nms <- self$names()
+      for (i in names(private)) {
+
+      }
+      private$.labels <-
+    },
+
+    data = function() {
+      private$.data
+    },
+
+    associations = function(which = NULL) {
+      res <- private$.associations
+      if (!is.null(which) && !is.null(res)) {
+        res <- res[match_all(which, res)]
+      }
+      res
+    },
+
+    labels = function(which = NULL) {
+      res <- private$.labels
+      if (!is.null(which) && !is.null(res)) {
+        res <- res[match_all(which, res)]
+      }
+      res
+    },
+
+    config = function(which = NULL) {
+      res <- private$.config
+      if (!is.null(which) && !is.null(res)) {
+        res <- res[match_all(which, names(res))]
+      }
+      res
+    },
+
+    dictionary = function(which = NULL) {
+      res <- private$.dictionary
+      if (!is.null(which) && !is.null(res)) {
+        res <- res[match_all(which, names(res))]
+      }
+      res
     },
 
     bind_rows = function(...) {
-      data <- c(list(self$.data), list(...))
-      data <- lapply(data, function(x) {
-        if (is.survey(x))
-          x <- x$.data
-        if (data.table::is.data.table(x)) {
-          data.table::copy(x)
-        } else {
-          x
-        }
-      })
-      self$.data <- do.call("rbind", data)
+      data <- c(list(private$.data), list(...))
+      data <- lapply(data, function(x) { if (is.survey(x)) x$data() else x })
+      private$.data <- do.call("rbind", data)
     },
 
     bind_cols = function(...) {
-      data <- c(list(self$.data), list(...))
-      data <- lapply(data, function(x) {
-        if (is.survey(x))
-          x <- x$.data
-        if (data.table::is.data.table(x)) {
-          data.table::copy(x)
-        } else {
-          x
-        }
-      })
-      self$.data <- do.call("cbind", data)
+      data <- c(list(private$.data), list(...))
+      data <- lapply(data, function(x) { if (is.survey(x)) x$data() else x })
+      private$.data <- do.call("cbind", data)
     },
 
     names = function(...) {
       "Return the name of columns in the data."
-      names(self$.data)
+      names(private$.data)
     },
 
     bracket = function(...) {
-      `[`(self$.data, ...)
+      `[`(private$.data, ...)
     },
 
     dbracket = function(...) {
-      `[[`(self$.data, ...)
+      `[[`(private$.data, ...)
     },
 
     bracket_repl = function(i, j, value) {
-      `[<-`(self$.data, i, j, value)
+      `[<-`(private$.data, i, j, value)
     },
 
     dbracket_repl = function(i, j, value) {
-      `[[<-`(self$.data, i, j, value)
+      `[[<-`(private$.data, i, j, value)
     },
 
     print = function(...) {
-      print(self$.data)
+      print(private$.data)
     }
   )
 )
@@ -123,22 +154,41 @@ survey_df <- function(x) Survey_df$new(x)
 }
 
 rbind.Survey <- function(x, ...) {
+  x <- x$clone(deep = FALSE)
   x$bind_rows(...)
 }
 
 cbind.Survey <- function(x, ...) {
+  x <- x$clone(deep = FALSE)
   x$bind_cols(...)
 }
 
 names.Survey <- function(x) x$names()
 
+y <- data.table::as.data.table(x)
 df <- survey_df(x)
 dt <- survey_dt(x)
 
-test <- rbind(dt, df); class(test); nrow(test)
-test <- rbind(df, df, df); class(test); nrow(test)
 test <- rbind(dt, df, df); class(test); nrow(test)
-
-test <- cbind(dt, df); class(test); ncol(test)
-test <- cbind(df, df, df); class(test); ncol(test)
 test <- cbind(dt, df, df); class(test); ncol(test)
+
+
+
+
+
+# BENCHMARKS
+library(microbenchmark)
+
+srv_dt_c <- function() cbind(dt, dt, dt)
+srv_df_c <- function() cbind(df, df, df)
+dt_c <- function() cbind(y, y, y)
+df_c <- function() cbind(x, x, x)
+
+microbenchmark(srv_dt_c(), srv_df_c(), dt_c(), df_c())
+
+srv_dt_r <- function() rbind(dt, dt, dt)
+srv_df_r <- function() rbind(df, df, df)
+dt_r <- function() rbind(y, y, y)
+df_r <- function() rbind(x, x, x)
+
+microbenchmark(srv_dt_r(), srv_df_r(), dt_r(), df_r())
