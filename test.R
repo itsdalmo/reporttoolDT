b8b2c06cb31edd00c05f211de0837d1ea901f0e5
dalmo@surface <kristian@doingit.no>
2016-02-24 15:56:47 +0100

    Testing

--- test.R
+++ test.R
@@ -18,11 +18,3 @@ data.table::address(test3$data)
 data.table::address(test1) # 1 and 3 are identical. I.e., updated by reference.
 data.table::address(test2)
 data.table::address(test3)
-
-
-test <- survey_df(x)
-test1 <- test
-test2 <- test[, "EPSI", drop = FALSE]
-
-data.table::address(test1)
-data.table::address(test2)
