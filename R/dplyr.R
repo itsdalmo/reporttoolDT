c499a83e2941818ef8951b80e2c76bc4c4f4bec3
dalmo@osx <kristian@doingit.no>
2016-03-02 22:31:19 +0100

    Cleaning and _each_ methods.

--- R/dplyr.R
+++ R/dplyr.R
@@ -2,14 +2,26 @@
 tbl_vars.Survey <- function(x) x$names()
 
 #' @export
-groups.Survey <- function(x) {
-  f <- get("groups", asNamespace("dplyr"))
+select_.Survey <- function(x, ...) {
+  f <- get("select_", asNamespace("dplyr"))
+  x$do(f, lazyeval::all_dots(..., all_named = FALSE), assign = FALSE)
+}
+
+#' @export
+rename_.Survey <- function(x, ...) {
+  f <- get("rename_", asNamespace("dplyr"))
   x$do(f, lazyeval::all_dots(..., all_named = TRUE), assign = FALSE)
 }
 
 #' @export
-ungroup.Survey <- function(x) {
-  f <- get("ungroup", asNamespace("dplyr"))
+filter_.Survey <- function(x, ...) {
+  f <- get("filter_", asNamespace("dplyr"))
+  x$do(f, lazyeval::all_dots(..., all_named = TRUE), assign = FALSE)
+}
+
+#' @export
+arrange_.Survey <- function(x, ...) {
+  f <- get("arrange_", asNamespace("dplyr"))
   x$do(f, lazyeval::all_dots(..., all_named = TRUE), assign = FALSE)
 }
 
@@ -21,14 +33,14 @@ group_by_.Survey <- function(x, ..., add = FALSE) {
 }
 
 #' @export
-summarise_.Survey <- function(x, ...) {
-  f <- get("summarise_", asNamespace("dplyr"))
+groups.Survey <- function(x, ...) {
+  f <- get("groups", asNamespace("dplyr"))
   x$do(f, lazyeval::all_dots(..., all_named = TRUE), assign = FALSE)
 }
 
 #' @export
-arrange_.Survey <- function(x, ...) {
-  f <- get("arrange_", asNamespace("dplyr"))
+ungroup.Survey <- function(x, ...) {
+  f <- get("ungroup", asNamespace("dplyr"))
   x$do(f, lazyeval::all_dots(..., all_named = TRUE), assign = FALSE)
 }
 
@@ -39,20 +51,19 @@ mutate_.Survey <- function(x, ...) {
 }
 
 #' @export
-select_.Survey <- function(x, ...) {
-  f <- get("select_", asNamespace("dplyr"))
+mutate_each_.Survey <- function(x, ...) {
+  f <- get("mutate_each_", asNamespace("dplyr"))
   x$do(f, lazyeval::all_dots(..., all_named = TRUE), assign = FALSE)
 }
 
 #' @export
-rename_.Survey <- function(x, ...) {
-  # TODO
-  f <- get("rename_", asNamespace("dplyr"))
+summarise_.Survey <- function(x, ...) {
+  f <- get("summarise_", asNamespace("dplyr"))
   x$do(f, lazyeval::all_dots(..., all_named = TRUE), assign = FALSE)
 }
 
 #' @export
-filter_.Survey <- function(x, ...) {
-  f <- get("filter_", asNamespace("dplyr"))
+summarise_each_.Survey <- function(x, ...) {
+  f <- get("summarise_each_", asNamespace("dplyr"))
   x$do(f, lazyeval::all_dots(..., all_named = TRUE), assign = FALSE)
 }
