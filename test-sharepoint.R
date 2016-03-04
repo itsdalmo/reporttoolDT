7b2122d64572ca67f5300336a365b259ab2ebfe4
dalmo@surface <kristian@doingit.no>
2016-03-04 15:30:11 +0100

    Added Hadley's suggestion for persistent (per session) login information.

--- test-sharepoint.R
+++ test-sharepoint.R
@@ -19,3 +19,27 @@ read_sp <- function(lnk, destination = NULL) {
   res
 
 }
+
+
+# github_pat <- function(force = FALSE) {
+#   env <- Sys.getenv('GITHUB_PAT')
+#   if (!identical(env, "") && !force) return(env)
+#
+#   if (!interactive()) {
+#     stop("Please set env var GITHUB_PAT to your github personal access token",
+#          call. = FALSE)
+#   }
+#
+#   message("Couldn't find env var GITHUB_PAT. See ?github_pat for more details.")
+#   message("Please enter your PAT and press enter:")
+#   pat <- readline(": ")
+#
+#   if (identical(pat, "")) {
+#     stop("Github personal access token entry failed", call. = FALSE)
+#   }
+#
+#   message("Updating GITHUB_PAT env var to PAT")
+#   Sys.setenv(GITHUB_PAT = pat)
+#
+#   pat
+# }
