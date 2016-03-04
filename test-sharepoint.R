93b2024b121c823a779edb97544bea705138628e
dalmo@surface <kristian@doingit.no>
2016-03-04 15:07:57 +0100

    Working HTTR approach to reading sharepoint. Can it also walk paths to read a survey?

---
+++ test-sharepoint.R
@@ -0,0 +1,21 @@
+# WORKING
+library(httr)
+test <- GET(lnk, authenticate("", "", type = "any"), write_disk(""))
+test <- GET(lnk, authenticate("", "", type = "ntlm"), write_disk(""))
+
+read_sp <- function(lnk, destination = NULL) {
+  if (is.null(destination)) {
+    destination <- tempfile(fileext = ".xlsx")
+    on.exit(unlink(destination, recursive = TRUE, force = TRUE), add = TRUE)
+  }
+
+  # Todo - persistent "cookies" or something?
+  usr <- readline("Enter username: ")
+  psw <- readline("Enter password: ")
+
+  # Todo - clean URL? Necessary or does httr handle it internally?
+  resp <- httr::GET(lnk, authenticate(usr, psw, type = "ntlm"), httr::write_disk(destination))
+  res <- readxl::read_excel(destination)
+  res
+
+}
