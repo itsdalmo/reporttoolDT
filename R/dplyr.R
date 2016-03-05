16cf176b1e6ea6987d1224a06a3879cd63b3d5e3
dalmo@pwin <kristian@doingit.no>
2016-03-05 17:20:01 +0100

    Quick cleanup.

--- R/dplyr.R
+++ R/dplyr.R
@@ -3,9 +3,9 @@ tbl_vars.Survey <- function(x) x$names()
 
 #' @export
 select_.Survey <- function(x, ...) {
-  dots <- lazyeval::all_dots(..., all_named = FALSE)
-
-  # dplyr::select allows renaming variables when called. (e.g., select(x, new_name = old_var))
+  # dplyr::select also allows renaming variables when called.
+  # e.g., select(x, new_name = old_var)
+  dots <- lazyeval::all_dots(...)
   vars <- renamed_vars(dots)
   if (length(vars))
     vars <- replace_all(names(x), vars)
@@ -28,54 +28,54 @@ rename_.Survey <- function(x, ...) {
 #' @export
 filter_.Survey <- function(x, ...) {
   f <- get("filter_", asNamespace("dplyr"))
-  x$do(f, lazyeval::all_dots(..., all_named = TRUE), assign = FALSE)
+  x$do(f, lazyeval::all_dots(...), assign = FALSE)
 }
 
 #' @export
 arrange_.Survey <- function(x, ...) {
   f <- get("arrange_", asNamespace("dplyr"))
-  x$do(f, lazyeval::all_dots(..., all_named = TRUE), assign = FALSE)
+  x$do(f, lazyeval::all_dots(...), assign = FALSE)
 }
 
 #' @export
 group_by_.Survey <- function(x, ..., add = FALSE) {
   # TODO: "add" is not passed to next call. Error message.
   f <- get("group_by_", asNamespace("dplyr"))
-  x$do(f, lazyeval::all_dots(..., all_named = TRUE), assign = FALSE)
+  x$do(f, lazyeval::all_dots(...), assign = FALSE)
 }
 
 #' @export
 groups.Survey <- function(x, ...) {
   f <- get("groups", asNamespace("dplyr"))
-  x$do(f, lazyeval::all_dots(..., all_named = TRUE), assign = FALSE)
+  x$do(f, lazyeval::all_dots(...), assign = FALSE)
 }
 
 #' @export
 ungroup.Survey <- function(x, ...) {
   f <- get("ungroup", asNamespace("dplyr"))
-  x$do(f, lazyeval::all_dots(..., all_named = TRUE), assign = FALSE)
+  x$do(f, lazyeval::all_dots(...), assign = FALSE)
 }
 
 #' @export
 mutate_.Survey <- function(x, ...) {
   f <- get("mutate_", asNamespace("dplyr"))
-  x$do(f, lazyeval::all_dots(..., all_named = TRUE), assign = FALSE)
+  x$do(f, lazyeval::all_dots(...), assign = FALSE)
 }
 
 #' @export
 mutate_each_.Survey <- function(x, ...) {
   f <- get("mutate_each_", asNamespace("dplyr"))
-  x$do(f, lazyeval::all_dots(..., all_named = TRUE), assign = FALSE)
+  x$do(f, lazyeval::all_dots(...), assign = FALSE)
 }
 
 #' @export
 summarise_.Survey <- function(x, ...) {
   f <- get("summarise_", asNamespace("dplyr"))
-  x$do(f, lazyeval::all_dots(..., all_named = TRUE), assign = FALSE)
+  x$do(f, lazyeval::all_dots(...), assign = FALSE)
 }
 
 #' @export
 summarise_each_.Survey <- function(x, ...) {
   f <- get("summarise_each_", asNamespace("dplyr"))
-  x$do(f, lazyeval::all_dots(..., all_named = TRUE), assign = FALSE)
+  x$do(f, lazyeval::all_dots(...), assign = FALSE)
 }
