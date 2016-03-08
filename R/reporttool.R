1079a8346dbaeecd03a36bb79b62796d141d0cd2
dalmo@pwin <kristian@doingit.no>
2016-03-08 12:35:30 +0100

    Use importFrom instead of import for stringi.

--- R/reporttool.R
+++ R/reporttool.R
@@ -1,4 +1,4 @@
-#' @import stringi
+#' @importFrom stringi stri_c stri_detect stri_replace stri_sub stri_length stri_pad stri_enc_tonative stri_trans_totitle stri_trans_tolower
 
 #' @export
 .datatable.aware <- TRUE
