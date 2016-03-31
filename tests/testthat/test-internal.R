abc808f29e46f5eaa2156b73b99a5c31b0edba9a
dalmo@pwin <kristian@doingit.no>
2016-03-31 15:15:34 +0200

    Removed functions and dependencies that are part of officeR.

--- tests/testthat/test-internal.R
+++ tests/testthat/test-internal.R
@@ -1,10 +1,2 @@
 context("Internal functions")
 
-test_that("intranet_link", {
-  if (!on_windows()) {
-    expect_warning(intranet_link("https://sp.com/test/"))
-  }
-
-  expect_error(intranet_link("sp.com/test/"))
-  expect_identical(intranet_link("https://sp.com/test/"), "\\\\sp.com@SSL/DavWWWRoot/test/")
-})
