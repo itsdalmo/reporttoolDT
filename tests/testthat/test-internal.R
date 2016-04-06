d87516f9e50b09ba9779017115ca5da028c96a5e
dalmo@osx <kristian@doingit.no>
2016-04-06 16:02:00 +0200

    Tests for internal functions, including updated tests for clean_score - which now extracts the FIRST 1-2 digit number in a string.

--- tests/testthat/test-internal.R
+++ tests/testthat/test-internal.R
@@ -1,2 +1,30 @@
 context("Internal functions")
 
+test_that("str_list", {
+  expect_error(str_list(1))
+  expect_identical(str_list("A"), "A")
+  expect_identical(str_list(c("A", "B")), "A and B")
+})
+
+test_that("str_just", {
+  x <- paste(letters[1:10], collapse = "")
+  expect_error(str_just(1))
+  expect_identical(str_just(x, n = 8), "abcde...")
+  expect_identical(str_just(x, n = 11, pad = " "), "abcdefghij ")
+  expect_identical(str_just(NA_character_), NA_character_)
+})
+
+test_that("clean scores", {
+
+  x <- c("1 Not happy", "Very happy 10", "Invalid 11", "Don't know")
+  expect_identical(clean_score(x), c(1L, 10L, NA, NA))
+
+})
+
+test_that("rescaling scores", {
+
+  x <- c("1 Not happy", "Very happy 10", "Invalid 11", "Don't know")
+  expect_identical(clean_score(x), c(1L, 10L, NA, NA))
+  expect_identical(rescale_100(clean_score(x)), c(0, 100, NA, NA))
+
+})
