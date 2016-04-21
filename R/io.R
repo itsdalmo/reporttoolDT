2a6c110bc37d9921342d898f28ef2dc62cfc079c
dalmo@pwin <kristian@doingit.no>
2016-04-21 14:38:07 +0200

    Draft IO-functions for Surveys. TODO: Separate functions for directories/standalone output?

--- R/io.R
+++ R/io.R
@@ -1,85 +1,10 @@
-#' Read a Survey
-#'
-#' The preferred way to store a \code{Survey} is by using \code{write_survey},
-#' which persists all the information in the \code{Survey}. Information that
-#' cannot be stored in SPSS for instance, will be stored in an associated .Rdata
-#' file instead. This also means that you have to use \code{\link{read_survey}}
-#' to get everything back.
-#'
-#' You can also use \code{\link[officeR]{write_data}} to write the \code{Survey}.
-#' In this case the information which is persisted depends on the format used:
-#'
-#' \itemize{
-#'  \item \code{.sav}: Data including labels, and levels for factor variables.
-#'  \item \code{.xlsx}: Data, measurement model (labels and levels) and entities.
-#'  \item \code{.Rdata}: Everything is stored.
-#' }
-#'
-#' @param x A \code{Survey}.
-#' @param file Output file or directory.
-#' @param write_input Set to \code{TRUE} to create input-files for the PLS-wizard.
-#' @param ... Arguments passed to \code{\link[officeR]{write_data}}.
-#' @author Kristian D. Olsen
-#' @export
-#' @examples
-#' \dontrun{
-#' df <- survey_df(data.frame("A" = "test", "B" = 2))
-#'
-#' # Store data and labels
-#' officeR::write_data(df, "test.sav")
-#'
-#' # Store everything
-#' write_survey(df, "test.Rdata")
-#' }
-
-read_survey <- function(file, mainentity = "q1", ...) {
-
-}
-
-read_model_output <- function(file, mainentity = "q1") {
-  file <- clean_path(file)
-  if (!file.exists(file))
-    stop("File does not exist:\n", file)
-
-  folders <- c("Data", "Input", "Output")
-  folders <- required_folders(file, folders, create = FALSE)
-
-  # 1 - Read the .sav file -----------------------------------------------------
-  fname <- list.files(folders$data)
-  fname <- fname[stri_detect(fname, regex = ".*em\\.sav$", case_insensitive = TRUE)]
-  if (length(fname) != 1L) {
-    msg <- if (length(fname) > 1L) "Found more than one" else "Could not find any"
-    stop(stri_c(msg, "'...EM.sav' files.", sep = " "))
-  }
-
-  data <- read_data(fname)
-
-  # 1.1 - Include fields if they exist
-  fname <- stri_c(tools::file_path_sans_ext(fname), " (survey).Rdata")
-  if (file.exists(fname)) {
-    fields <- read_data(fname)
-  } else {
-    fields <- NULL
-  }
-
-  # 1.2 - Convert to Survey
-  srv <- Survey$new(data, fields = fields)
-
-  # 2 - Read input -------------------------------------------------------------
-
-
-  file <- list.files(dir)
-  file <- file[stri_detect(stri_trans_tolower(file), regex = ".*em\\.sav$")]
-
-}
-
 #' Write a Survey
 #'
-#' The preferred way to store a \code{Survey} is by using \code{write_survey},
-#' which persists all the information in the \code{Survey}. Information that
-#' cannot be stored in SPSS for instance, will be stored in an associated .Rdata
-#' file instead. This also means that you have to use \code{\link{read_survey}}
-#' to get everything back.
+#' In contrast to \code{\link[officeR]{write_data}}, \code{write_survey} will store
+#' hidden fields (such as config, translations etc.) which can be retrieved by using
+#' \code{\link{read_survey}}. This means that you don't have to repeatedly set
+#' these options when reading/writing a survey you are working on. Information that
+#' cannot be stored in SPSS will be stored in an associated .Rdata file instead.
 #'
 #' You can also use \code{\link[officeR]{write_data}} to write the \code{Survey}.
 #' In this case the information which is persisted depends on the format used:
@@ -91,9 +16,8 @@ read_model_output <- function(file, mainentity = "q1") {
 #' }
 #'
 #' @param x A \code{Survey}.
-#' @param file Output file or directory.
-#' @param write_input Set to \code{TRUE} to create input-files for the PLS-wizard.
-#' @param ... Arguments passed to \code{\link[officeR]{write_data}}.
+#' @param file Output file or directory. (If a directory is specified, PLS-input
+#' will be created. The resulting EM.sav file-name is based on the Survey's config.)
 #' @author Kristian D. Olsen
 #' @export
 #' @examples
@@ -107,42 +31,113 @@ read_model_output <- function(file, mainentity = "q1") {
 #' write_survey(df, "test.Rdata")
 #' }
 
-write_survey <- function(x, file, write_input = FALSE) {
-  stopifnot(is.survey(x))
+write_survey <- function(x, file) {
+  if (!is.survey(x)) stop("Use officeR::write_data to write data that is not a survey.")
   file <- clean_path(file)
 
+  # Use extension to determine output
   ext <- stri_trans_tolower(tools::file_ext(file))
   if (ext == "") {
-    required <- if (write_input) c("Data", "Input") else "Data"
-    folders <- required_folders(file, required, create = TRUE)
-
-    # Make sure the necessary configs have been set.
-    cfg <- x$get_config(c("name", "segment", "year"))
-    if (is.null(cfg) || is.na(cfg[["name"]]))
-      stop("Config must be set and name cannot be empty.")
-
-    # Construct a file name from config.
-    data <- stri_c(stri_c(cfg[!is.na(cfg)], collapse = " "), ".sav")
-    folders$data <- file.path(folders$data, data)
-
-  } else if (write_input) {
-    stop("Cannot write input when 'file' is not a directory.")
+    # Return early. Writing model input also writes the data.
+    write_model_input(x$clone(deep = TRUE), file)
   } else if (ext != "sav") {
-    stop("Use write_data to write Surveys to formats other than .sav.")
+    stop("Expecting a directory or .sav file. Other formats can be written with officeR.")
   } else {
-    folders <- list(data = file)
+    # Write .sav file (Long strings backup is created by officeR.)
+    officeR::write_data(x, file)
+
+    # Include hidden fields.
+    file <- stri_c(tools::file_path_sans_ext(file), " (survey).Rdata")
+    officeR::write_data(x$get_field(), file)
   }
 
-  # Write input
-  if (write_input)
-    write_model_input(x$clone(deep = TRUE), folders$input)
+  # Make sure nothing is printed
+  invisible()
 
-  # Write hidden fields.
-  rdata_file <- stri_c(tools::file_path_sans_ext(folders$data), " (survey).Rdata")
-  write_data(x$get_field(), file = rdata_file)
+}
 
-  # Write data
-  write_data(x, file = folders$data)
+# Function to write input-files for the PLS-wizard.
+# (recursively calls write_survey to write the .sav file.)
+write_model_input <- function(x, file) {
+  x <- survey_dt(x)
+
+  # 1 - Use config to creat a filename -----------------------------------------
+  cfg <- x$get_config(c("name", "segment", "year"))
+  if (is.null(cfg)) stop("Config must be set before writing input.")
+  fname <- stri_c(stri_c(cfg[!is.na(cfg)], collapse = " "), ".sav")
+
+  # 2 - Check data -------------------------------------------------------------
+  # Make sure mainentity is set and that it is a factor variable
+  me <- unname(x$get_association("mainentity")); lvl <- unique(x[[me]])
+  if (is.null(me)) {
+    stop("'mainentity' must be set before writing input.")
+  } else if (!is.factor(lvl)) {
+    lvl <- lvl[stri_order(lvl)]
+    x[, (me) := lapply(.SD, function(x) factor(x, levels = me_levels)), .SDcols = me]
+  }
+
+  # Check whether required variables can be found. (order by variable names)
+  mm <- x$get_association(which = get_default("latents"))
+  mm <- mm[match_all(names(x), mm)]
+
+  cr <- names(x)[stri_trans_tolower(names(x)) %in% "coderesp"]
+  if (!length(cr)) stop("'coderesp' variable was not found.")
+  vars <- c(unname(mm), me, cr)
+
+  # 3 - Create required folders ------------------------------------------------
+  folders <- c("Data", "Input")
+  folders <- require_folders(file, folders, create = TRUE)
+
+  # 4 - Write data -------------------------------------------------------------
+  fname <- file.path(folders$data, fname)
+  write_survey(x, fname)
+
+  # 5 - Write measurement model ------------------------------------------------
+  model <- split(unname(mm), names(mm))
+  model <- lapply(model[get_default("latents")], function(vars) as.integer(mm %in% vars) * -1L)
+  model <- as.data.frame(c(list(manifest = unname(mm), model)), stringsAsFactors = FALSE)
+
+  fname <- file.path(folders$input, "measurement model.txt")
+  write.table(model, fname, sep = "\t", row.names = FALSE, quote = FALSE, fileEncoding = "latin1")
+
+  # 5 - Write PLS data ---------------------------------------------------------
+  mm <- unname(mm) # Drop latent association.
+  co <- as.numeric(x$get_config("cutoff"))
+  if (is.na(co) || !length(co)) {
+    warning("Cutoff had not been set. Defaulting to 30%.", call. = FALSE)
+    co <- .3; x$set_config(cutoff = .3)
+  }
+
+  data <- x$get_data()
+  data <- data[percent_missing <= co, vars, with = FALSE]
+
+  # Convert mainentity to integer, clean scales, recode NA and order by mainentity.
+  data[, (me) := lapply(.SD, as.integer), .SDcols = unname(me)]
+  data[, (mm) := lapply(.SD, clean_scale), .SDcols = mm]
+  data[, (mm) := lapply(.SD, function(x) { x[is.na(x)] <- 98; x }), .SDcols = mm]
+  data.table::setkeyv(data, me)
+
+  fname <- file.path(folders$input, "modelldatafil.txt")
+  write.table(data, fname, sep = "\t", row.names = FALSE, quote = FALSE, fileEncoding = "latin1")
+
+  # 6 - Write config (entities, count and marketshares) ------------------------
+  en <- x$entities() # Returns a data.frame.
+  en <- en[, c("entity", "valid", "marketshare")]
+
+  fname <- file.path(folders$input, "config.txt")
+  write.table(en, fname, sep = "\t", dec = ",", col.names = FALSE, quote = FALSE, fileEncoding = "latin1")
+
+  # 7 - Question text ----------------------------------------------------------
+  qt <- unname(x$get_label(which = mm))
+
+  fname <- file.path(folders$input, "qtext.txt")
+  write.table(qt, fname, row.names = FALSE, col.names = FALSE, quote = FALSE, fileEncoding = "latin1")
+
+  # 8 - Entity (Q1) names ------------------------------------------------------
+  q1 <- as.character(en$entity)
+
+  fname <- file.path(folders$input, "q1names.txt")
+  write.table(q1, fname, row.names = FALSE, col.names = FALSE, quote = FALSE, fileEncoding = "latin1")
 
   # Make sure nothing is printed
   invisible()
@@ -183,78 +178,118 @@ to_labelled.Survey <- function(x) {
 
 }
 
-# This function creates "input"-files for the PLS-wizard -----------------------
-write_model_input <- function(x, dir) {
-  # Convert Survey to Survey_dt
-  x <- survey_dt(x)
+#' Read a Survey
+#'
+#' Read a \code{Survey} that has been written by \code{\link{write_survey}} (or
+#' created manually using the PLS-wizard). This function also reads output from
+#' the PLS-wizard, such as inner and outer weights. Hidden fields will be updated
+#' to reflect changes in input-files when reading a survey directory.
+#'
+#' @param file Output file or directory.
+#' @param mainentity Name of the mainentity variable in data.
+#' @author Kristian D. Olsen
+#' @export
+#' @examples
+#' \dontrun{
+#' # TODO
+#' }
 
-  # Mainentity must be a factor variable
-  me <- unname(x$get_association("mainentity"))
-  if (!is.factor(x[[me]])) {
-    lvls <- unique(x[[me]])
-    lvls <- lvls[stri_order(lvls)]
-    # Use unique/ordered values as levels for mainentity.
-    x[, (me) := lapply(.SD, function(x) factor(x, levels = me_levels)), .SDcols = me]
+read_survey <- function(file, mainentity = "q1") {
+  file <- clean_path(file)
+  if (!file.exists(file)) stop("File does not exist:\n", file)
+
+  # Use extension to determine output
+  ext <- stri_trans_tolower(tools::file_ext(file))
+  if (ext == "") {
+    # Return early. Reading model output also reads in the data.
+    return(read_model_output(file, mainentity))
+  } else if (ext != "sav") {
+    stop("Expecting a directory or .sav file. Other formats can be read with officeR.")
   }
 
-  # Make sure data has been prepared beforehand
-  reqs <- c(unname(mm), me, "coderesp")
-  vars <- match_all(stri_trans_tolower(reqs), stri_trans_tolower(x$names()))
-  if (length(vars) != length(reqs)) stop("Could not find required variables in data. See help(prepare_data).")
+  # Read .sav file (Long strings are read by officeR.)
+  out <- officeR::read_data(file)
 
-  # 1 - Write measurement model ------------------------------------------------
-  mm <- x$get_association(which = get_default("latents"))
-  model <- split(unname(mm), names(mm))
-  model <- lapply(model[unique(names(mm))], function(vars) as.integer(mm %in% vars) * -1L)
-  model <- as.data.frame(c(list(manifest = unname(mm), model)), stringsAsFactors = FALSE)
-
-  fname <- file.path(dir, "measurement model.txt")
-  write.table(model, fname, sep = "\t", row.names = FALSE, quote = FALSE, fileEncoding = "latin1")
-
-  # 2 - Write model data -------------------------------------------------------
-  mm <- unname(mm) # Drop latent association.
-  co <- as.numeric(x$get_config("cutoff"))
-  if (is.na(co)) {
-    co <- .3; x$set_config(cutoff = .3)
+  # Include hidden fields if they exist.
+  file <- stri_c(tools::file_path_sans_ext(file), " (survey).Rdata")
+  if (file.exists(file)) {
+    fields <- officeR::read_data(file)
+  } else {
+    fields <- NULL
   }
 
-  data <- x$get_data()
-  data <- data[percent_missing <= co, vars, with = FALSE]
+  out <- Survey_tbl$new(out, fields = fields) # Always return tbl?
 
-  # Convert mainentity to integer, clean scales, recode NA and order by mainentity.
-  data[, (me) := lapply(.SD, as.integer), .SDcols = me]
-  data[, (mm) := lapply(.SD, clean_scale), .SDcols = mm]
-  data[, (mm) := lapply(.SD, function(x) { x[is.na(x)] <- 98; x }), .SDcols = mm]
-  data.table::setkeyv(data, me)
+  # Rename "andel_missing" to "percent_missing".
+  if (!"percent_missing" %in% stri_trans_tolower(names(out))) {
+    names(out)[stri_trans_tolower(names(out)) == "andel_missing"] <-  "percent_missing"
+  }
 
-  fname <- file.path(dir, "modelldatafil.txt")
-  write.table(data, fname, sep = "\t", row.names = FALSE, quote = FALSE, fileEncoding = "latin1")
-
-  # 3 - Config (entities, count and marketshares) ------------------------------
-  en <- x$entities() # Returns a data.frame.
-  en <- en[, c("entity", "valid", "marketshare")]
-
-  fname <- file.path(dir, "config.txt")
-  write.table(en, fname, sep = "\t", dec = ",", col.names = FALSE, quote = FALSE, fileEncoding = "latin1")
-
-  # 4 - Question text ----------------------------------------------------------
-  qt <- unname(x$get_label(which = mm))
-
-  fname <- file.path(dir, "qtext.txt")
-  write.table(qt, fname, row.names = FALSE, col.names = FALSE, quote = FALSE, fileEncoding = "latin1")
-
-  # 5 - Entity (Q1) names ------------------------------------------------------
-  q1 <- levels(x[[me]])
-
-  fname <- file.path(dir, "q1names.txt")
-  write.table(q1, fname, row.names = FALSE, col.names = FALSE, quote = FALSE, fileEncoding = "latin1")
-
-  # Make sure nothing is printed
-  invisible()
+  # Return
+  out
 
 }
 
-required_folders <- function(path, folders, create = FALSE) {
+# Function to read input to/output from the PLS-wizard.
+# (recursively calls read_survey to read the .sav file.)
+read_model_output <- function(file, mainentity) {
+  # Match required folders (case_insensitive.)
+  folders <- c("Data", "Input", "Output")
+  folders <- require_folders(file, folders, create = FALSE)
+
+  # 1 - Read the .sav file -----------------------------------------------------
+  files <- file.path(folders$data, list.files(folders$data))
+  fname <- files[stri_detect(files, regex = ".*em\\.sav$", case_insensitive = TRUE)]
+  if (length(fname) != 1L) {
+    msg <- if (length(fname) > 1L) "Found more than one" else "Could not find any"
+    stop(stri_c(msg, "'...EM.sav' files.", sep = " "))
+  }
+
+  # Call read_survey again, using the path to the .sav.
+  out <- read_survey(fname, mainentity = mainentity)
+
+  # 2 - Read model -------------------------------------------------------------
+  files <- file.path(folders$input, list.files(folders$input))
+  fname <- files[stri_detect(files, regex = "measurement model.*\\.txt", case_insensitive = TRUE)]
+  if (length(fname) != 1L) {
+    msg <- if (length(fname) > 1L) "Found more than one" else "Could not find any"
+    stop(stri_c(msg, "file matching 'measurement model.txt'", sep = " "))
+  }
+
+  mm <- officeR::read_data(fname, encoding = "latin1", col_types = list("Manifest" = readr::col_character()))
+  mm <- lapply(mm[-1], function(x, vars) {vars[x == -1]}, mm[[1]])
+
+  # Case insensitive matching for mainentity.
+  mainentity <- names(out)[tolower(names(out)) %in% tolower(mainentity)]
+
+  # Set associations specified in measurement model (and mainentity).
+  out$set_association(list = mm, mainentity = mainentity)
+
+  # 3 - Read config ------------------------------------------------------------
+  fname <- files[stri_detect(files, regex = "config.*\\.txt", case_insensitive = TRUE)]
+  if (length(fname) != 1L) {
+    msg <- if (length(fname) > 1L) "Found more than one" else "Could not find any"
+    stop(stri_c(msg, "file matching 'config.txt'", sep = " "))
+  }
+
+  cf <- officeR::read_data(fname, encoding = "latin1", col_names = FALSE, decimal = ",")
+  names(cf) <- c("row", "entity", "valid", "marketshare")
+
+  # Set marketshare based on config
+  out$set_marketshare(list = setNames(as.list(cf$marketshare), cf$entity))
+
+  # 4 - Read inner/outer weights -----------------------------------------------
+
+  # TODO
+
+  # Return
+  out
+
+}
+
+# Match contents from a path to a list of required folders (case insensitive)
+# (Returns a named list with paths to the folders. Optionally creates missing folders.)
+require_folders <- function(path, folders, create = FALSE) {
   if (missing(folders) || !is.character(folders))
     stop("Argument 'folders' should be a character vector.")
 
@@ -262,7 +297,7 @@ required_folders <- function(path, folders, create = FALSE) {
   existing <- list.files(path)
   missing <- folders[!tolower(folders) %in% tolower(existing)]
   if (length(missing)) {
-    if (!create) stop("Required folders do not exist:\n", str_list(missing))
+    if (!create) stop("Required folders do not exist:\n", str_list(missing), call. = FALSE)
     for (folder in missing) {
       dir.create(file.path(path, folder), showWarnings = FALSE)
     }
