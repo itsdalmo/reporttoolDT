b989ae7499564b2b76654e8f60716807dfc83e34
Kristian <dalmo@surface>
2016-05-27 14:23:22 +0200

    Added rescale_10 to complement rescale_100, and as_scale to convert variables to factors with labels ordered according to numerals in labels.

--- R/internal.R
+++ R/internal.R
@@ -32,8 +32,9 @@ clean_scale <- function(var) {
 #' Takes vectors representing 10-point likert scales and transforms them to
 #' 100-point scales (\code{numeric}). Formula: \code{(x-1)*(100/9)}
 #'
-#' @param var A numeric vector, \code{integer} and \code{character} will be coerced.
+#' @param var An \code{integer} vector.
 #' @author Kristian D. Olsen
+#' @seealso \code{\link{rescale_10}} for the complement.
 #' @export
 #' @examples
 #' x <- rescale_100(c(1L, 10L))
@@ -52,24 +53,83 @@ rescale_100 <- function(var) {
 
 }
 
-#' Conjunct strings
+#' Rescale 0-100 numeric to 1-10 integer.
 #'
-#' See examples.
+#' Takes vectors representing 100-point scales and transforms them to
+#' 10-point likert-scales (\code{integer}).
 #'
-#' @param x A character vector.
-#' @param conjunction The conjunction to use.
-#' @param quote Put quotes ('') around list items. Default is \code{TRUE}.
+#' @param var A \code{numeric} vector.
 #' @author Kristian D. Olsen
+#' @seealso \code{\link{rescale_100}} for the complement.
 #' @export
 #' @examples
-#' x <- str_list(c("A", "B", "C"), conjunction = "or")
-#' identical(x, c("'A', 'B' or 'C'"))
+#' x <- rescale_100(c(1L, 10L))
+#' x <- rescale_10(x)
+#' identical(x, c(1L, 10L))
 
-str_list <- function(x, conjunction = "and", quote = TRUE) {
-  stopifnot(is.character(x))
-  if (length(x) == 1L) return(x)
-  if (quote) x <- stri_c("'", x, "'")
-  stri_c(stri_c(x[1:(length(x)-1)], collapse = ", "), conjunction, x[length(x)], sep = " ")
+rescale_10 <- function(var) {
+  if (!is.numeric(var))
+    stop("'var' should be numeric.")
+
+  # Rescale from 10 point to 100 point scale
+  var <- (var-0L)/100L * 9L + 1L
+
+  # Set values other than 0 - 100 as NA. Return
+  var[var > 10L | var < 1L] <- NA
+  as.integer(var)
+}
+
+#' Convert variable to scale
+#'
+#' Takes a character or factor variable and converts it to a factor with labels
+#' ordered by numerals in the label. Labels with duplicated numerals will be
+#' merged in the output.
+#'
+#' Note: \code{as_scale} is convenient for converting likert scales from \code{character}
+#' to \code{factor}. This tends to happend when merging data with e.g.
+#' \code{\link[dplyr]{bind_rows}}.
+#'
+#' @param var A \code{numeric} vector.
+#' @author Kristian D. Olsen
+#' @seealso \code{\link{rescale_10}} to convert 100-point scales to 10-point.
+#' @export
+#' @examples
+#' NULL
+
+as_scale <- function(var) {
+  UseMethod("as_scale")
+}
+
+#' @export
+as_scale.numeric <- function(var) {
+  if (!is.integer(var))
+    stop("'var' should be an integer. Use rescale_10() to convert 100-point scales.", call. = FALSE)
+  labs <- unique(var)[order(unique(var))]
+  factor(as.character(var), levels = as.character(labs))
+}
+
+#' @export
+as_scale.character <- function(var) {
+  # Get labels and numerals in labels
+  lab <- unique(var)
+  num <- str_to_numeric(lab, FUN = stop, " <- ", "Labels contain multiple numerals.")
+  out <- factor(var, levels = lab[order(num)])
+
+  # Also recode labels if there are any duplicates
+  is_dup <- duplicated(num) & !is.na(num)
+  if (!any(is_dup)) return(out)
+
+  for (i in num[is_dup]) {
+    ID <- num == i & !is.na(num)
+    out <- recode_(out, setNames(list(lab[ID]), lab[ID][1L]))
+  }
+
+  out
+}
+
+#' @export
+as_scale.factor <- function(var) {
+  as_scale(as.character(var))
 }
 
 #' Extract numbers from strings
@@ -109,6 +169,26 @@ str_to_numeric <- function(x, FUN = mean, ...) {
   # Return
   unlist(out)
 
+}
+
+#' Conjunct strings
+#'
+#' See examples.
+#'
+#' @param x A character vector.
+#' @param conjunction The conjunction to use.
+#' @param quote Put quotes ('') around list items. Default is \code{TRUE}.
+#' @author Kristian D. Olsen
+#' @export
+#' @examples
+#' x <- str_list(c("A", "B", "C"), conjunction = "or")
+#' identical(x, c("'A', 'B' or 'C'"))
+
+str_list <- function(x, conjunction = "and", quote = TRUE) {
+  stopifnot(is.character(x))
+  if (length(x) == 1L) return(x)
+  if (quote) x <- stri_c("'", x, "'")
+  stri_c(stri_c(x[1:(length(x)-1)], collapse = ", "), conjunction, x[length(x)], sep = " ")
 }
 
 #' Trim strings
--- tests/testthat/test-internal.R
+++ tests/testthat/test-internal.R
@@ -14,7 +14,7 @@ test_that("str_just", {
   expect_identical(str_just(NA_character_), NA_character_)
 })
 
-test_that("clean scores", {
+test_that("clean scale", {
 
   x <- c("1 Not happy", "Very happy 10", "Invalid 11", "Don't know")
   expect_identical(clean_scale(x), c(1L, 10L, NA, NA))
@@ -26,5 +26,27 @@ test_that("rescaling scores", {
   x <- c("1 Not happy", "Very happy 10", "Invalid 11", "Don't know")
   expect_identical(clean_scale(x), c(1L, 10L, NA, NA))
   expect_identical(rescale_100(clean_scale(x)), c(0, 100, NA, NA))
+  expect_identical(rescale_10(rescale_100(clean_scale(x))), c(1L, 10L, NA, NA))
 
+})
+
+test_that("as_scale works", {
+  x <- c("10 such happy", "1 Not happy", "1 Not at all happy", "Don't know")
+  y <- as_scale(x)
+  expect_identical(levels(y), c("1 Not happy", "10 such happy", "Don't know"))
+  expect_identical(as.character(y), c("10 such happy", "1 Not happy", "1 Not happy", "Don't know"))
+})
+
+test_that("as_scale also works with numerics", {
+  x <- c(1L, 3L, 2L)
+  expect_error(as_scale(as.numeric(x)), "should be an integer")
+  y <- as_scale(x)
+  expect_identical(as.character(y), c("1", "3", "2"))
+  expect_identical(levels(y), c("1", "2", "3"))
+})
+
+test_that("as_scale does not touch labels if no numerals are found", {
+  x <- c("Don't know", "wow")
+  y <- as_scale(x)
+  expect_identical(y, as.factor(x))
 })
