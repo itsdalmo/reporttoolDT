657f612a0af28aced3fc9981c6700b09470d5379
dalmo@pwin <kristian@doingit.no>
2016-06-10 08:26:39 +0200

    Add head/tail to imports.

--- R/reporttool.R
+++ R/reporttool.R
@@ -35,7 +35,7 @@
 #' @author Kristian D. Olsen
 #' @importFrom stats setNames
 #' @importFrom stringi stri_c stri_detect stri_replace stri_sub stri_length stri_pad stri_enc_tonative stri_trans_totitle stri_trans_tolower
-#' @importFrom utils write.table
+#' @importFrom utils head tail write.table
 #' @docType package
 #' @name reporttoolDT
 
