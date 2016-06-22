425d3355f311ee47c6a573ae2e3fab6bc900619a
dalmo@osx <kristian@doingit.no>
2016-06-22 12:49:19 +0200

    WIP: generate_report using the new render_ functions.

--- R/report.R
+++ R/report.R
@@ -2,8 +2,13 @@
 #'
 #' This function is a wrapper for \code{\link[rmarkdown]{render}} with HTML output.
 #'
-#' @inheritParams render_pdf
-#' @param ... Further arguments passed to \code{\link{html_template}}.
+#' @param x An object to pass to the knitr environment when processing.
+#' @param input Path to a \code{.Rmd} report template.
+#' @param group A variable to "group" the reports by. Each entity will get their
+#' own report.
+#' @param type What format should the output be in. PDF, PPT or HTML.
+#' @param output Optional directory to use for output files.
+#' @param ... Further arguments passed to the render functions.
 #' @author Kristian D. Olsen
 #' @export
 #' @examples
@@ -12,7 +17,7 @@
 #'   render_html("example.Rmd")
 #' }
 
-generate_report <- function(x, rmd = NULL, entity = NULL, type = c("pdf", "ppt", "html"), path = NULL, ...) {
+generate_report <- function(x, input = NULL, group = NULL, output = NULL, type = c("pdf", "ppt", "html"), ...) {
   if (!requireNamespace("knitr", quietly = TRUE))
     stop("'knitr' is required for generating PDF's.", quietly = TRUE)
   if (!requireNamespace("rmarkdown", quietly = TRUE))
@@ -21,23 +26,23 @@ generate_report <- function(x, rmd = NULL, entity = NULL, type = c("pdf", "ppt",
   type <- stri_trans_tolower(type)
   type <- match.arg(type, several.ok = FALSE)
 
-  # If rmd is not specified, use default template. If dir is not specified,
-  # use the same directory as the rmd file, or working directory if rmd is NULL.
-  if (is.null(rmd)) {
-    if (!is.survey(x)) stop("'x' must be a Survey when rmd is NULL (default template).")
-    rmd <- system.file("rmarkdown/templates/report_template/skeleton/skeleton.Rmd", package = "reporttoolDT")
-    path <- path %||% getwd()
+  # If input is not specified, use default template. If dir is not specified,
+  # use the same directory as the input file, or working directory if input is NULL.
+  if (is.null(input)) {
+    if (!is.survey(x)) stop("'x' must be a Survey when input is NULL (default template).")
+    input <- system.file("rmarkdown/templates/report_template/skeleton/skeleton.Rmd", package = "reporttoolDT")
+    output <- output %||% getwd()
   } else {
-    rmd <- clean_path(rmd)
-    ext <- stri_trans_tolower(tools::file_ext(rmd))
-    if (ext != "rmd") stop("Argument rmd should be a path to a .Rmd file:\n", rmd)
-    path <- path %||% dirname(rmd)
+    input <- clean_output(input)
+    ext <- stri_trans_tolower(tools::file_ext(input))
+    if (ext != "rmd") stop("Argument input should be a path to a .Rmd file:\n", input)
+    output <- output %||% dirname(input)
   }
 
   # Make sure the Markdown and output directory exists, and the theme files.
   output_dir <- switch(type, pdf = "PDF", html = "HTML", ppt = "Powerpoint")
-  dir.create(file.path(path, output_dir), showWarnings = FALSE)
-  dir.create(file.path(path, "Markdown"), showWarnings = FALSE)
+  dir.create(file.path(output, output_dir), showWarnings = FALSE)
+  dir.create(file.path(output, "Markdown"), showWarnings = FALSE)
 
   # Create a new environment which contains 'x'.
   if (is.survey(x)) {
@@ -51,7 +56,7 @@ generate_report <- function(x, rmd = NULL, entity = NULL, type = c("pdf", "ppt",
   }
 
   # TODO: How should entities be handled if specified? Why doesn't x[[ent]] work?
-  # Create one report per entity for Surveys.
+  # Create one report per group for Surveys.
   if (is.survey(x)) {
     # Only create reports for entities which have observations.
     ent <- get_association(x, "mainentity")
@@ -60,7 +65,7 @@ generate_report <- function(x, rmd = NULL, entity = NULL, type = c("pdf", "ppt",
     ent <- ent[stringi::stri_order(ent)]
   } else {
     # If 'x' is not a survey, create one report based on the Rmd template.
-    ent <- basename_sans_ext(rmd)
+    ent <- basename_sans_ext(input)
   }
 
   input <- c(input, list(entity = ent))
