e250e3675f7268cd11a65b03682f788113339cff
Kristian <dalmo@surface>
2016-06-27 13:49:18 +0200

    Working write_questionnaire (internal).

--- R/internal.R
+++ R/internal.R
@@ -25,6 +25,19 @@ write_questionnaire <- function(x, file, study = NULL, segment = NULL, year = NU
   if (!is.data.frame(x)) {
     stop("'x' (the questionnaire) should be a data.frame.", call. = FALSE)
   }
+
+  if (is.null(year)) {
+    year <- as.numeric(format(Sys.Date(), "%Y"))
+  } else if (!is.numeric(year)) {
+    stop("'year' should be numeric.", call. = FALSE)
+  }
+
+  args <- list(study = study, segment = segment, entity = entity)
+  not_string <- !vapply(args, is_string, logical(1)) & !vapply(args, is.null, logical(1))
+  if (any(not_string)) {
+    stop("The following arguments should be a string:\n", str_list(names(args)[not_string]), call. = FALSE)
+  }
+
   # Convert to regular data.frame and lowercase names
   if (data.table::is.data.table(x))
     x <- as.data.frame(x)
@@ -34,28 +47,31 @@ write_questionnaire <- function(x, file, study = NULL, segment = NULL, year = NU
   exp <- c("year", "segment", "study", "latent", "order", "manifest", "primer", "question", "type", "values", "conditional_on")
   missing <- setdiff(exp, names(x))
   if (length(missing)) {
-    stop("Could not find the following expected columns:\n", str_list(missing), call. = FALSE)
+    stop("Could not find the following (required) columns:\n", str_list(missing), call. = FALSE)
   }
 
-  # Lowercase filter-columns
-  x[, c("year", "segment", "study")] <- lapply(x[, c("year", "segment", "study")], stri_trans_tolower)
-
   # Filter ---------------------------------------------------------------------
-  # Study
+  # Lowercase filter-columns
+  x$year <- as.numeric(x$year)
+  x[, c("segment", "study")] <- lapply(x[, c("segment", "study")], stri_trans_tolower)
+
+  # Filter study
   if (is.null(study) && length(unique(x$study)) > 1L) {
     stop("'study' cannot be NULL when 'x' contains more than one study.", call. = FALSE)
   } else if (!study %in% x$study) {
     stop("The following 'study' was not found in the data:\n", str_list(study), call. = FALSE)
+  } else if (!is.null(study)) {
+    x <- x[x$study == stri_trans_tolower(study), ]
   }
-  x <- x[x$study == study, ]
 
   # Segment
   if (is.null(segment) && length(unique(x$segment)) > 1L) {
     stop("'segment' cannot be NULL when the study contains more than one segment.", call. = FALSE)
-  } else if (!segment %in% x$segment) {
+  } else if (!stri_trans_tolower(segment) %in% x$segment) {
     stop("The following 'segment' was not found for the current study:\n", str_list(segment), call. = FALSE)
+  } else if (!is.null(segment)) {
+    x <- x[x$segment == stri_trans_tolower(segment), ]
   }
-  x <- x[x$segment == segment, ]
 
   # Year
   if (!year %in% x$year) {
@@ -68,85 +84,77 @@ write_questionnaire <- function(x, file, study = NULL, segment = NULL, year = NU
 
   # Write ----------------------------------------------------------------------
 
-#
-#   # Subset and create index
-#   quest <- quest[stri_trans_tolower(quest$study) == study & stri_trans_tolower(quest$segment) == segment, ]
-#   quest[] <- lapply(quest, stri_replace_all, replacement = "", regex = "\\r")
-#   quest$index <- NA
-#
-#   # Expand scale variable levels
-#   is_scale <- stri_trans_tolower(quest$type) == "scale"
-#   quest$values[is_scale] <-  vapply(quest$values[is_scale], function(x) {
-#     x <- split_scale(x); stri_c(x[1], "...", x[10], if (length(x) > 10) x[11] else "", sep = "\n")
-#   }, character(1))
-#
-#   # Replace {XX} with whatever value is specified
-#   if (!is.null(entity)) {
-#     quest[] <- lapply(quest, stri_replace_all, replacement = entity, regex = "\\{XX\\}")
-#   }
-#
-#   # Loop through the rows and update the index
-#   for (i in 1:nrow(quest)) {
-#
-#     # Get the next index value
-#     index_value <- if (all(is.na(quest$index))) 1 else max(quest$index, na.rm = TRUE) + 1
-#
-#     # If index and primer are NA - use the rownumber as index
-#     if (is.na(quest$primer[i]) && is.na(quest$index[i])) {
-#       quest$index[i] <- index_value
-#
-#       # If only the index is NA - give the same index to all identical primers
-#     } else if (is.na(quest$index[i])) {
-#       quest$index[quest$primer %in% quest$primer[i]] <- index_value
-#     }
-#
-#   }
-#
-#   # Split the questionnaire by index
-#   split_quest <- split(quest, quest$index)
-#
-#   # Use the split list to write to a xlsx_file
-#   wb <- openxlsx::createWorkbook()
-#
-#   # Write the data and retain rows that have been written to
-#   merge_rows <- lapply(split_quest, function(x) {
-#
-#     # Get columns
-#     df <- x[, c("latent", "manifest", "question", "values")]
-#
-#     # Set either the primer or question as title
-#     if (nrow(x) == 1L && is.na(x$primer)) {
-#       title <- x$question
-#     } else {
-#       title <- stri_c(unique(x$primer), collapse = "\n")
-#     }
-#
-#     # Write the question
-#     to_sheet(df, wb, title = title, sheet = study)
-#
-#   })
-#
-#   lapply(merge_rows, function(x) {
-#
-#     rows <- x["first"]:x["last"]
-#
-#     # Merge values for question matrix
-#     if (length(rows) > 2L) {
-#       openxlsx::mergeCells(wb, sheet = study, cols = 4, rows = rows[-1])
-#     }
-#
-#     # Set values to wrap text
-#     openxlsx::addStyle(wb, sheet = study, style = openxlsx::createStyle(wrapText = TRUE),
-#                        rows = rows[-1], cols = 4, gridExpand = TRUE, stack = TRUE)
-#
-#   })
-#
-#   # Widen the columns containing the question text and values
-#   openxlsx::setColWidths(wb, sheet = study, cols = 3, widths = 100)
-#   openxlsx::setColWidths(wb, sheet = study, cols = 4, widths = 50)
-#
-#   # Save and make sure nothing is printed
-#   openxlsx::saveWorkbook(wb, file, overwrite = TRUE)
-#   invisible()
+  # Remove all \r newlines before writing and add an index.
+  x[] <- lapply(x, stri_replace_all, replacement = "", fixed = "\r")
+
+  # Expand scale variable levels
+  is_scale <- stri_trans_tolower(x$type) == "scale"
+  x$values[is_scale] <-  vapply(x$values[is_scale], function(v) {
+    v <- unlist(stri_split(v, fixed = "\n"))
+    if (length(v) == 1L) {
+      v
+    } else {
+      stri_c(v[1], "...", v[2], if (length(v) > 2L) v[3] else "", sep = "\n")
+    }
+  }, character(1))
+
+  # Replace {XX} with whatever value is specified
+  if (!is.null(entity)) {
+    x[] <- lapply(x, stri_replace_all, replacement = entity, fixed = "{XX}")
+  }
+
+  # Loop through the rows and update the index
+  x$index <- NA_real_
+  for (i in 1:nrow(x)) {
+    if (all(is.na(x$index))) {
+      index_value <- 1L
+    } else {
+      index_value <- max(x$index, na.rm = TRUE) + 1
+    }
+    # If index and primer are NA - use the rownumber as index
+    if (is.na(x$primer[i]) && is.na(x$index[i])) {
+      x$index[i] <- index_value
+    # If only the index is NA - give the same index to all identical primers
+    } else if (is.na(x$index[i])) {
+      x$index[x$primer %in% x$primer[i]] <- index_value
+    }
+  }
+
+  blocks <- split(x, x$index)
+  wb <- seamless::excel_workbook()
+
+  # Write the data and retain rows that have been written to
+  block_index <- lapply(blocks, function(q) {
+    df <- q[, c("latent", "manifest", "question", "values")]
+    # Set either the primer or question as title
+    if (nrow(q) == 1L && is.na(q$primer)) {
+      title <- q$question
+    } else {
+      title <- stri_c(unique(q$primer), collapse = "\n")
+    }
+    # Write the question
+    seamless::to_excel(df, wb, title = title, sheet = study)
+  })
+
+  lapply(block_index, function(x) {
+    rows <- x$rows["start"]:x$rows["end"]
+    # Merge values for question matrix
+    if (length(rows) > 3L) {
+      openxlsx::mergeCells(wb, sheet = study, cols = 4, rows = tail(rows, -2L))
+    }
+    # Always wrap text on values
+    openxlsx::addStyle(
+      wb, sheet = study, style = openxlsx::createStyle(wrapText = TRUE),
+      rows = tail(rows, -2L), cols = 4, gridExpand = TRUE, stack = TRUE)
+
+  })
+
+  # Widen the columns containing the question text and values
+  openxlsx::setColWidths(wb, sheet = study, cols = 3, widths = 100)
+  openxlsx::setColWidths(wb, sheet = study, cols = 4, widths = 50)
+
+  # Save and make sure nothing is printed
+  seamless::write_data(wb, file, overwrite = TRUE)
+  invisible()
 
 }
